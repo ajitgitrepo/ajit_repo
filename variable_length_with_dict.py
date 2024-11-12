9ccb7fc8e79f6eafb666540257e745a2e4ed4ae9
Ajit Nayak <oracle.ajitnayak@gmail.com>
2024-11-12 17:03:05 +0530

    first commit python program variable_length_with_dict.py

---
+++ variable_length_with_dict.py
@@ -0,0 +1,13 @@
+def person(name,*data):
+    print(name)
+    print(data)
+
+person('ajit',33,'kuligan',9841149863)
+
+def person_with_dict(name, **data):
+    print(name)
+    for i,j in data.items():
+        print(i,j)
+
+        
+person_with_dict('ajit',age=33, city='kuligan', mob=9841149863)
